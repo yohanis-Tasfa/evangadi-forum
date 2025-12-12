d1f273d3801097a51819fb5a0f1ed29b022395fe
yohanis tasfa <yohanistesfa457@gmail.com>
2025-12-12 19:58:45 +0300

    modified

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,24 +1,22 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-async function authmiddleware(req,res,next){
-    const authHeader = req.headers.authorization
+async function authmiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
 
-    if(!authHeader || !authHeader.startsWith('Bearer') ) {
-        return res.status(401).json({msg:"invalid authentication"})
-    }
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res.status(401).json({ msg: "invalid authentication" });
+  }
 
-    const  token = authHeader.split(' ')[1]
-    console.log(authHeader)
-    console.log(token)
+  const token = authHeader.split(" ")[1];
+  console.log(authHeader);
+  console.log(token);
 
-    try {
-        const {username,userid} = jwt.verify(token,process.env.JWT_SECRET)
-        req.user = {username,userid}
-        next()
-        
-    } catch (error) {
-        return res.status(401).json({msg:"invalid authentication"})
-    }
+  try {
+    const { username, userid } = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { username, userid };
+    next();
+  } catch (error) {
+    return res.status(401).json({ msg: "invalid authentication" });
+  }
 }
-
-module.exports = authmiddleware
+module.exports = authmiddleware;
--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -1,24 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // import auth middleware
-const authmiddleware = require("../middleware/authmiddleware")
+const authmiddleware = require("../middleware/authmiddleware");
 
-const {createAnswer,allAnswer,specificAnswer,updateAnswer,deleteAnswer} = require("../controller/answerController")
+const {
+  createAnswer,
+  allAnswer,
+  specificAnswer,
+  updateAnswer,
+  deleteAnswer,
+} = require("../controller/answerController");
 
 // Create answer
-router.post('/create', authmiddleware,createAnswer);
+router.post("/create", authmiddleware, createAnswer);
 
 // Get all answers for a question
-router.get('/question/:questionid',authmiddleware,allAnswer);
+router.get("/question/:questionid", authmiddleware, allAnswer);
 
 // Get answers of a specific user
-router.get('/user/:userid',authmiddleware, specificAnswer);
+router.get("/user/:userid", authmiddleware, specificAnswer);
 
 // Update answer
-router.put('/update/:answerid',authmiddleware, updateAnswer);
+router.put("/update/:answerid", authmiddleware, updateAnswer);
 
 // Delete answer
-router.delete('/delete/:answerid',authmiddleware, deleteAnswer);
+router.delete("/delete/:answerid", authmiddleware, deleteAnswer);
 
 module.exports = router;
